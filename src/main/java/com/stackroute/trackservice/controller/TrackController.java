8bd815e2b5040f974d70c804f0b7586b4f15a158
sivadatta.bathina <sivadatta.bathina@cgi.com>
2019-08-02 12:47:35 +0530

    Added Swagger2.

--- src/main/java/com/stackroute/trackservice/controller/TrackController.java
+++ src/main/java/com/stackroute/trackservice/controller/TrackController.java
@@ -2,6 +2,7 @@ package com.stackroute.trackservice.controller;
 
 import com.stackroute.trackservice.domain.Track;
 import com.stackroute.trackservice.service.TrackService;
+import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
