4443a1b7a035ececba0268085957f83007b4ebc2
sivadatta.bathina <sivadatta.bathina@cgi.com>
2019-08-02 12:10:48 +0530

    Updated Endpoint in TrackController for searchTrackByName

--- src/main/java/com/stackroute/trackservice/controller/TrackController.java
+++ src/main/java/com/stackroute/trackservice/controller/TrackController.java
@@ -56,7 +56,7 @@ public class TrackController {
      * @param trackName Name of the track to search.
      * @return List of tracks
      */
-    @GetMapping("tracks/name/{trackName}")
+    @GetMapping("tracks/{trackName}")
     public ResponseEntity<?> searchTrackByName(@PathVariable String trackName) {
         List<Track> foundTracksList = trackService.searchTrackByName(trackName);
         return new ResponseEntity<>(foundTracksList, HttpStatus.FOUND);
