4cf856548e119f3afd2a77b82a15db7dc0229879
sivadatta.bathina <sivadatta.bathina@cgi.com>
2019-08-05 01:06:40 +0530

    Removed getters and setters in Track. Added Lombok annotations.

--- src/main/java/com/stackroute/trackservice/domain/Track.java
+++ src/main/java/com/stackroute/trackservice/domain/Track.java
@@ -1,5 +1,8 @@
 package com.stackroute.trackservice.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
@@ -11,42 +14,12 @@ import javax.persistence.Id;
  * like trackId, trackName, comments.
  */
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class Track {
     @Id
     private int trackId;
     private String trackName;
     private String comments;
-
-    public Track() {
-    }
-
-    public Track(int trackId, String trackName, String comments) {
-        this.trackId = trackId;
-        this.trackName = trackName;
-        this.comments = comments;
-    }
-
-    public int getTrackId() {
-        return trackId;
-    }
-
-    public void setTrackId(int trackId) {
-        this.trackId = trackId;
-    }
-
-    public String getTrackName() {
-        return trackName;
-    }
-
-    public void setTrackName(String trackName) {
-        this.trackName = trackName;
-    }
-
-    public String getComments() {
-        return comments;
-    }
-
-    public void setComments(String comments) {
-        this.comments = comments;
-    }
 }
